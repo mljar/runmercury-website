df273664e8d771ce2315dd37d38d9e42750de0b8
Piotr Plonski <pplonski86@gmail.com>
2023-02-14 14:49:23 +0100

    add examples

---
+++ components/icons/click.tsx
@@ -0,0 +1,21 @@
+export default (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    stroke-width="2"
+    stroke="currentColor"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M3 12l3 0"></path>
+    <path d="M12 3l0 3"></path>
+    <path d="M7.8 7.8l-2.2 -2.2"></path>
+    <path d="M16.2 7.8l2.2 -2.2"></path>
+    <path d="M7.8 16.2l-2.2 2.2"></path>
+    <path d="M12 12l9 3l-4 2l-2 4l-3 -9"></path>
+  </svg>
+);
