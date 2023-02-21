011f28f328085a1393e39880b899923ffaa59b5b
Piotr <pplonski86@gmail.com>
2023-02-21 10:15:54 +0100

    add stop execution example

--- components/icons/handStop.tsx
+++ components/icons/handStop.tsx
@@ -4,11 +4,11 @@ export default (
     width="24"
     height="24"
     viewBox="0 0 24 24"
-    stroke-width="2"
+    strokeWidth="2"
     stroke="currentColor"
     fill="none"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeLinecap="round"
+    strokeLinejoin="round"
   >
     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
     <path d="M8 13v-7.5a1.5 1.5 0 0 1 3 0v6.5"></path>
