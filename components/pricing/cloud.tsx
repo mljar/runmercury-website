cde49137e2a060e9d832816e5f657781b63cabff
Piotr <pplonski86@gmail.com>
2023-03-16 13:15:13 +0100

    update pricing

---
+++ components/pricing/cloud.tsx
@@ -0,0 +1,201 @@
+
+import tickIcon from "../icons/tick";
+
+export default (
+  <div className="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0 px-16">
+    <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
+      <h3 className="mb-4 text-2xl font-semibold">Starter</h3>
+      <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+        Start for free today! No credit card required 
+      </p>
+      <div className="flex justify-center items-baseline my-8">
+        <span className="mr-2 text-5xl font-extrabold">$0</span>
+        <span className="text-gray-500 dark:text-gray-400">/month</span>
+      </div>
+
+      <ul role="list" className="mb-8 space-y-4 text-left">
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>
+            Setup only <span className="font-semibold">1 Mercury Site</span>
+          </span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>
+            <span className="font-semibold">Public</span> or{" "}
+            <span className="font-semibold">Private</span> notebooks
+          </span>
+        </li>
+
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>
+            Invite <span className="font-semibold">unlimited users</span>
+          </span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>
+            Upload up to <span className="font-semibold">10 files</span>
+          </span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>
+            Max file size <span className="font-semibold">10MB</span>
+          </span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>
+            <span className="font-semibold">5 hours of</span> worker / monthly
+          </span>
+        </li>
+      </ul>
+      <a
+        href="https://forms.gle/EjqYi3ttEkZkuKy46"
+        target="_blank"
+        className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900"
+      >
+        Notify when ready
+      </a>
+    </div>
+
+    <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
+      <h3 className="mb-4 text-2xl font-semibold">Pro</h3>
+      <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+        Best option for professionals with deadlines 
+      </p>
+      <div className="flex justify-center items-baseline my-8">
+        <span className="mr-2 text-5xl font-extrabold">$20</span>
+        <span className="text-gray-500 dark:text-gray-400">/month</span>
+      </div>
+
+      <ul role="list" className="mb-8 space-y-4 text-left">
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>
+            Setup up to <span className="font-semibold">3 Mercury Sites</span>
+          </span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>
+            <span className="font-semibold">Public</span> or{" "}
+            <span className="font-semibold">Private</span> notebooks
+          </span>
+        </li>
+
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>
+            Invite <span className="font-semibold">unlimited users</span>
+          </span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>
+            Upload up to <span className="font-semibold">25 files</span> per
+            Site
+          </span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>
+            Max file size <span className="font-semibold">50MB</span>
+          </span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>
+            <span className="font-semibold">25 hours of</span> worker / monthly
+          </span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>Style customization</span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>Custom domains</span>
+        </li>
+      </ul>
+      <a
+        href="https://forms.gle/EjqYi3ttEkZkuKy46"
+        target="_blank"
+        className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900"
+      >
+        Notify when ready
+      </a>
+    </div>
+
+    <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
+      <h3 className="mb-4 text-2xl font-semibold">Business</h3>
+      <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+        Data teams that need to share more notebooks{" "}
+      </p>
+      <div className="flex justify-center items-baseline my-8">
+        <span className="mr-2 text-5xl font-extrabold">$100</span>
+        <span className="text-gray-500 dark:text-gray-400">/month</span>
+      </div>
+
+      <ul role="list" className="mb-8 space-y-4 text-left">
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>
+            Setup up to <span className="font-semibold">5 Mercury Sites</span>
+          </span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>
+            <span className="font-semibold">Public</span> or{" "}
+            <span className="font-semibold">Private</span> notebooks
+          </span>
+        </li>
+
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>
+            Invite <span className="font-semibold">unlimited users</span>
+          </span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>
+            Upload up to <span className="font-semibold">50 files</span> per
+            Site
+          </span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>
+            Max file size <span className="font-semibold">100MB</span>
+          </span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>
+            <span className="font-semibold">250 hours of</span> worker / monthly
+          </span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>Style customization</span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>Custom domains</span>
+        </li>
+      </ul>
+      <a
+        href="https://forms.gle/EjqYi3ttEkZkuKy46"
+        target="_blank"
+        className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900"
+      >
+        Notify when ready
+      </a>
+    </div>
+  </div>
+);
---
+++ components/pricing/self.tsx
@@ -0,0 +1,186 @@
+
+import tickIcon from "../icons/tick";
+
+export default (
+  <div className="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0 px-16">
+    <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
+      <h3 className="mb-4 text-2xl font-semibold">Free</h3>
+      <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+        Deploy Mercury and share notebooks on your servers
+      </p>
+      <div className="flex justify-center items-baseline my-8">
+        <span className="mr-2 text-5xl font-extrabold">$0</span>
+        <span className="text-gray-500 dark:text-gray-400"></span>
+      </div>
+
+      <ul role="list" className="mb-8 space-y-4 text-left">
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>Deployment on <span className="font-semibold">single</span> machine</span>
+        </li>
+
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>
+            <span className="font-semibold">Unlimited Sites</span>
+          </span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>
+            <span className="font-semibold">Unlimited users</span>
+          </span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>
+            <span className="font-semibold">Unlimited files</span>
+          </span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>Custom domains</span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>Community support</span>
+        </li>
+
+      </ul>
+
+      <a
+        href="https://forms.gle/EjqYi3ttEkZkuKy46"
+        target="_blank"
+        className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900"
+      >
+        Notify when ready
+      </a>
+    </div>
+
+    <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
+      <h3 className="mb-4 text-2xl font-semibold">Commercial</h3>
+      <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+        Customize Mercury and serve notebook on your servers
+      </p>
+      <div className="flex justify-center items-baseline my-8">
+        <span className="mr-2 text-5xl font-extrabold">$1,000</span>
+        <span className="text-gray-500 dark:text-gray-400">/year</span>
+      </div>
+
+      <ul role="list" className="mb-8 space-y-4 text-left">
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>Deployment on <span className="font-semibold">single</span> machine</span>
+        </li>
+
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>
+            <span className="font-semibold">Unlimited Sites</span>
+          </span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>
+            <span className="font-semibold">Unlimited users</span>
+          </span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>
+            <span className="font-semibold">Unlimited files</span>
+          </span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>Custom domains</span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>Email support</span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>Style customization</span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>Private forks</span>
+        </li>
+
+      </ul>
+
+      
+      <a
+        href="https://forms.gle/EjqYi3ttEkZkuKy46"
+        target="_blank"
+        className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900"
+      >
+        Notify when ready
+      </a>
+    </div>
+
+    <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
+      <h3 className="mb-4 text-2xl font-semibold">Enterprise</h3>
+      <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
+        Use multi-server deployment for large organizations
+      </p>
+      <div className="flex justify-center items-baseline my-8">
+        <span className="mr-2 text-5xl font-extrabold">$10,000</span>
+        <span className="text-gray-500 dark:text-gray-400">/month</span>
+      </div>
+
+      <ul role="list" className="mb-8 space-y-4 text-left">
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>Deployment on <span className="font-semibold">multiple</span> machines</span>
+        </li>
+
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>
+            <span className="font-semibold">Unlimited Sites</span>
+          </span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>
+            <span className="font-semibold">Unlimited users</span>
+          </span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>
+            <span className="font-semibold">Unlimited files</span>
+          </span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>Custom domains</span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>Email support</span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>Style customization</span>
+        </li>
+        <li className="flex items-center space-x-3">
+          {tickIcon}
+          <span>Private forks</span>
+        </li>
+
+      </ul>
+
+      <a
+        href="https://forms.gle/EjqYi3ttEkZkuKy46"
+        target="_blank"
+        className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900"
+      >
+        Notify when ready
+      </a>
+    </div>
+
+  </div>
+);
