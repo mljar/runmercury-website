0f3358458838f64fe8321aa6f195b7c44539573a
Piotr <pplonski86@gmail.com>
2023-04-05 16:19:43 +0200

    add links to cloud

--- components/pricing/cloud.tsx
+++ components/pricing/cloud.tsx
@@ -54,11 +54,11 @@ export default (
         </li>
       </ul>
       <a
-        href="https://forms.gle/EjqYi3ttEkZkuKy46"
+        href="https://cloud.runmercury.com/register"
         target="_blank"
         className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900"
       >
-        Notify when ready
+        Sign up
       </a>
     </div>
 
@@ -122,11 +122,11 @@ export default (
         </li>
       </ul>
       <a
-        href="https://forms.gle/EjqYi3ttEkZkuKy46"
+        href="https://cloud.runmercury.com/register"
         target="_blank"
         className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900"
       >
-        Notify when ready
+        Sign up
       </a>
     </div>
 
@@ -190,11 +190,11 @@ export default (
         </li>
       </ul>
       <a
-        href="https://forms.gle/EjqYi3ttEkZkuKy46"
+        href="https://cloud.runmercury.com/register"
         target="_blank"
         className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900"
       >
-        Notify when ready
+        Sign up
       </a>
     </div>
   </div>
