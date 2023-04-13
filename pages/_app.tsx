e39678ce914b86b6f7a291186b174840330fb879
Piotr Plonski <pplonski86@gmail.com>
2023-04-13 17:14:53 +0200

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,31 @@
-import type { ReactElement } from 'react'
-import type { AppProps } from 'next/app'
+import type { ReactElement } from "react";
+import type { AppProps } from "next/app";
 
-import '../style.css'
+import Script from "next/script";
+
+import "../style.css";
 
 export default function Nextra({
   Component,
-  pageProps
+  pageProps,
 }: AppProps): ReactElement {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-718XMSDDTR"
+      ></Script>
+
+      <Script strategy="afterInteractive">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+              
+                    gtag('config', 'G-718XMSDDTR');
+                `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
