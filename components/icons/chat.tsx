12e7e0a9e844e6f53257dede78bcb867782ff403
Piotr <pplonski86@gmail.com>
2023-04-28 15:18:06 +0200

    add ngrok  and confetti docs

---
+++ components/icons/chat.tsx
@@ -0,0 +1,18 @@
+export default (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    strokeWidth="2"
+    stroke="currentColor"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M8 9h8"></path>
+    <path d="M8 13h6"></path>
+    <path d="M18 4a3 3 0 0 1 3 3v8a3 3 0 0 1 -3 3h-5l-5 3v-3h-2a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12z"></path>
+  </svg>
+);
