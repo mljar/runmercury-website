0c18134d08247b7ed1497016cf6ef3eb1680e940
Olunia <olunia@laptok.local>
2023-05-27 21:38:59 +0200

    add numberbox docs (#6)

---
+++ components/icons/squareNumber.tsx
@@ -0,0 +1,18 @@
+export default (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      strokeWidth="2"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M3 3m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z"></path>
+        <path d="M10 10l2 -2v8"></path> 
+    </svg>
+  );
+  
