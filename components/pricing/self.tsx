335b2db5349e49fe8e8cbacfecf02ad7dacb80ed
Olunia <olunia@laptok.local>
2023-05-27 12:21:04 +0200

    small updates

--- components/pricing/self.tsx
+++ components/pricing/self.tsx
@@ -49,11 +49,11 @@ export default (
       </ul>
 
       <a
-        href="https://forms.gle/EjqYi3ttEkZkuKy46"
+        href="/docs/"
         target="_blank"
         className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900"
       >
-        Notify when ready
+        Check docs
       </a>
     </div>
 
@@ -112,11 +112,10 @@ export default (
 
       
       <a
-        href="https://forms.gle/EjqYi3ttEkZkuKy46"
-        target="_blank"
+        href="/support/contact/"
         className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900"
       >
-        Notify when ready
+        Contact us
       </a>
     </div>
 
@@ -174,11 +173,10 @@ export default (
       </ul>
 
       <a
-        href="https://forms.gle/EjqYi3ttEkZkuKy46"
-        target="_blank"
+        href="/support/contact/"
         className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900"
       >
-        Notify when ready
+        Contact us
       </a>
     </div>
 
