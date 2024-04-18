09d3bfa9b2b506190e786e099e77bc2f84109006
apjanusz <januszekaptacy2606@gmail.com>
2024-04-18 14:49:12 +0200

    add base to integrations

--- components/icons/barChart.tsx
+++ components/icons/barChart.tsx
@@ -4,16 +4,16 @@ export default (
     width="24"
     height="24"
     viewBox="0 0 24 24"
-    strokeWidth="2"
-    stroke="currentColor"
     fill="none"
-    strokeLinecap="round"
-    strokeLinejoin="round"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
   >
-    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-    <path d="M14 3v4a1 1 0 0 0 1 1h4"></path>
-    <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z"></path>
-    <path d="M12 11v6"></path>
-    <path d="M9.5 13.5l2.5 -2.5l2.5 2.5"></path>
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M3 12m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v6a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" />
+    <path d="M9 8m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v10a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" />
+    <path d="M15 4m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" />
+    <path d="M4 20l14 0" />
   </svg>
 );
