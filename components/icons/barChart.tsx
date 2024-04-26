922ba181739be783a764242cccc6266ac6cf2bc8
apjanusz <januszekaptacy2606@gmail.com>
2024-04-26 13:08:57 +0200

    fix react svg icons

--- components/icons/barChart.tsx
+++ components/icons/barChart.tsx
@@ -6,9 +6,9 @@ export default (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
   >
     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
     <path d="M3 12m0 1a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v6a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" />
--- components/icons/histogramChart.tsx
+++ components/icons/histogramChart.tsx
@@ -6,9 +6,9 @@ export default (
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
-    stroke-width="2"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
   >
     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
     <path d="M3 3v18h18" />
