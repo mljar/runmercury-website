2d562e26f98956682d2748a0c360cd8082fb1054
apjanusz <januszekaptacy2606@gmail.com>
2024-05-17 14:47:48 +0200

    add vega-altair area chart integration

---
+++ components/icons/areaChart.tsx
@@ -0,0 +1,18 @@
+export default (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="icon icon-tabler icons-tabler-outline icon-tabler-chart-area"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M4 19l16 0" />
+    <path d="M4 15l4 -6l4 2l4 -5l4 4l0 5l-16 0" />
+  </svg>
+);
